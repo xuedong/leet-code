858a100b1be2c27e747d7eb77f164008c44b3d1a
xuedong <shang.xuedong@yahoo.fr>
2020-10-05 00:38:30 +0200

    Add mergeSortedArray.kt

---
+++ Explore/Learn/Introduction to Data Structure/Arrays 101/Merge Sorted Array/mergeSortedArray.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var k = m+n-1
+        var i = m-1
+        var j = n-1
+        
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i]
+                i--
+            } else {
+                nums1[k--] = nums2[j]
+                j--
+            }
+        }
+        
+        while (i >= 0) {
+            nums1[k--] = nums1[i]
+            i--
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j]
+            j--
+        }
+    }
+}
+
