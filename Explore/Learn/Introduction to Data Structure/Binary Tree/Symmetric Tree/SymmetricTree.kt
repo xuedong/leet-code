558305fb0d7d9d1a7390095f423c51f104217c6b
xuedong <shang.xuedong@yahoo.fr>
2021-04-22 22:06:58 +0200

    Update binary tree
    Add symmetric tree in Kotlin

---
+++ Explore/Learn/Introduction to Data Structure/Binary Tree/Symmetric Tree/SymmetricTree.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun aux(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) return true
+        if (left == null && right != null) return false
+        if (left != null && right == null) return false
+        if (left!!.`val` != right!!.`val`) return false
+        return aux(left!!.left, right!!.right) && aux(left!!.right, right!!.left)
+    }
+    
+    fun isSymmetric(root: TreeNode?): Boolean {
+        if (root == null) return true
+        return aux(root!!.left, root!!.right)
+    }
+}
+
