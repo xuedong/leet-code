19e59396b810ea64458736dc66645d3154c22557
xuedong <shang.xuedong@yahoo.fr>
2021-04-22 22:01:40 +0200

    Update binary tree
    Add symmetric tree in Java

---
+++ Explore/Learn/Introduction to Data Structure/Binary Tree/Symmetric Tree/SymmetricTree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean aux(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null && right != null) return false;
+        if (left != null && right == null) return false;
+        if (left.val != right.val) return false;
+        return aux(left.left, right.right) && aux(left.right, right.left);
+    }
+    
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return aux(root.left, root.right);
+    }
+}
+
