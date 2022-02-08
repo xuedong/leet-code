4631cb3f4cc3b18ea7e5e7645f0e5dd2e4f8ef75
xuedong <shang.xuedong@yahoo.fr>
2022-02-08 02:29:44 +0100

    Add 258

---
+++ Problems/Algorithms/Add Digits/AddDigits.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun addDigits(num: Int): Int {
+        if (num == 0) return 0
+        
+        if (num % 9 == 0) {
+            return 9
+        } else {
+            return num % 9
+        }
+    }
+}
+
