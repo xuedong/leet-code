2e1639d21899edaefe03f86b30752825fbd50d18
xuedong <shang.xuedong@yahoo.fr>
2022-03-12 21:36:07 +0100

    Add 700

---
+++ Problems/Algorithms/700. Search in a Binary Search Tree/SearchBST.kt
@@ -0,0 +1,20 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) return null
+        
+        if (root?.`val` == `val`) return root
+        if (root?.`val` > `val`) return searchBST(root?.left, `val`)
+        return searchBST(root?.right, `val`)
+    }
+}
+
