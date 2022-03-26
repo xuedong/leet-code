bbcf4f29f4aa1635cd523c8d9fdb18b8cf27b9f0
xuedong <shang.xuedong@yahoo.fr>
2022-03-26 01:23:00 +0100

    Add 704

---
+++ Problems/Algorithms/704. Binary Search/BinarySearch.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var left = 0
+        var right = nums.size - 1
+        
+        while (left <= right) {
+            val mid = (left + right) / 2
+            if (nums[mid] == target) {
+                return mid
+            } else if (nums[mid] < target) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        
+        return -1
+    }
+}
+
