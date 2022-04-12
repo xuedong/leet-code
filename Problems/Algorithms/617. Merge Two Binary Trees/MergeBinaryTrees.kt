368c0cc3571a774cde0364ba2022fd0dabcdd655
xuedong <shang.xuedong@yahoo.fr>
2022-04-12 11:57:16 +0200

    Add 617

---
+++ Problems/Algorithms/617. Merge Two Binary Trees/MergeBinaryTrees.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
+        if (root1 == null) return root2
+        if (root2 == null) return root1
+        
+        val root = TreeNode(root1?.`val`+root2?.`val`)
+        root.left = mergeTrees(root1?.left, root2?.left)
+        root.right = mergeTrees(root1?.right, root2?.right)
+        
+        return root
+    }
+}
+
