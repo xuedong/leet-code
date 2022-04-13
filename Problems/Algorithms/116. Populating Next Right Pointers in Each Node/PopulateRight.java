9495011728f0e04910042790ff92bcc86dbceab0
xuedong <shang.xuedong@yahoo.fr>
2022-04-13 10:51:05 +0200

    Add some java solutions

---
+++ Problems/Algorithms/116. Populating Next Right Pointers in Each Node/PopulateRight.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    private void connectTwoNodes(Node node1, Node node2) {
+        if (node1 == null && node2 == null) return;
+        
+        node1.next = node2;
+        connectTwoNodes(node1.left, node1.right);
+        connectTwoNodes(node1.right, node2.left);
+        connectTwoNodes(node2.left, node2.right);
+    }
+
+    public Node connect(Node root) {
+        if (root == null) return null;
+        connectTwoNodes(root.left, root.right);
+        return root;
+    }
+}
+
