8f90ca72eddfa6fc6b839fa351e9d1945c1c358b
xuedong <shang.xuedong@yahoo.fr>
2022-04-21 11:22:47 +0200

    Add 608

---
+++ Problems/Database/608. Tree Node/tree_node.sql
@@ -0,0 +1,32 @@
+# Write your MySQL query statement below
+SELECT id, 'Root' AS type
+FROM Tree
+WHERE p_id IS NULL
+
+UNION
+
+SELECT id, 'Leaf' AS type
+FROM Tree
+WHERE
+    id NOT IN
+    (
+        SELECT DISTINCT p_id
+        FROM Tree
+        WHERE p_id IS NOT NULL
+    )
+    AND p_id IS NOT NULL
+    
+UNION
+
+SELECT id, 'Inner' AS type
+FROM Tree
+WHERE
+    id IN
+    (
+        SELECT DISTINCT p_id
+        FROM Tree
+        WHERE p_id IS NOT NULL
+    )
+    AND p_id IS NOT NULL
+;
+
