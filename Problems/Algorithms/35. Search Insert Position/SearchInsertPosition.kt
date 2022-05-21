dbbdc58088c2368e06bd499dedea11d65c891732
xuedong <shang.xuedong@yahoo.fr>
2022-05-21 12:22:47 +0200

    Add some java solutions

--- Problems/Algorithms/35. Search Insert Position/SearchInsertPosition.kt
+++ Problems/Algorithms/35. Search Insert Position/SearchInsertPosition.kt
@@ -2,20 +2,19 @@ class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
         var left = 0
         var right = nums.size - 1
-        
+
         while (left <= right) {
             val mid = left + (right - left) / 2
-            
+
             if (nums[mid] == target) {
                 return mid
             } else if (nums[mid] > target) {
-                right = mid - 1;
+                right = mid - 1
             } else {
-                left = mid + 1;
+                left = mid + 1
             }
         }
-        
-        return left;
+
+        return left
     }
 }
-
