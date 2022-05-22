2ce4614a4574db13ed3aa34a0cdf3df4520b07b0
xuedong <shang.xuedong@yahoo.fr>
2022-05-22 17:01:40 +0200

    Add 367

---
+++ Problems/Algorithms/367. Valid Perfect Square/ValidPerfectSquare.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun isPerfectSquare(num: Int): Boolean {
+        if (num == 1) return true
+        
+        var left = 0.toLong()
+        var right = num.toLong()
+        
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            
+            val curr = mid * mid
+            if (curr == num.toLong()) {
+                return true
+            } else if (curr < num.toLong()) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        
+        return false
+    }
+}
+
