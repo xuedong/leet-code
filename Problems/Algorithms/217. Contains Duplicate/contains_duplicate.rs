f3cd44fa43b41c7cded1c2b223ee827e8d74dab1
xuedong <shang.xuedong@yahoo.fr>
2022-06-16 18:42:03 +0200

    Add some rust solutions

---
+++ Problems/Algorithms/217. Contains Duplicate/contains_duplicate.rs
@@ -0,0 +1,8 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        nums.len() != nums.iter().collect::<HashSet<&i32>>().len()
+    }
+}
+
