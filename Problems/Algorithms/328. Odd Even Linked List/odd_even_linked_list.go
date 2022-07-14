1ee8cebde13936db3b4ae19041112997af60645b
xuedong <shang.xuedong@yahoo.fr>
2022-07-14 11:37:56 +0200

    Add 328

---
+++ Problems/Algorithms/328. Odd Even Linked List/odd_even_linked_list.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+    if head == nil {
+        return nil
+    }
+
+    odd, even := head, head.Next
+    even_head := even
+    for even != nil && even.Next != nil {
+        odd.Next = even.Next
+        odd = odd.Next
+        even.Next = odd.Next
+        even = even.Next
+    }
+    odd.Next = even_head
+
+    return head
+}
