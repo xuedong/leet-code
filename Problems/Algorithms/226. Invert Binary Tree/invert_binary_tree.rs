d002de00fa3ef0e8e48f03e2c0c3de7982bf06f3
xuedong <shang.xuedong@yahoo.fr>
2022-07-16 12:09:26 +0200

    Add some rust solutions

---
+++ Problems/Algorithms/226. Invert Binary Tree/invert_binary_tree.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        match root {
+            None => None,
+            Some(node) => {
+                let (left, right) = (Self::invert_tree(node.borrow().right.clone()), Self::invert_tree(node.borrow().left.clone()));
+                let node = node.clone();
+                node.borrow_mut().left = left;
+                node.borrow_mut().right = right;
+                Some(node)
+            }
+        }
+    }
+}
+
