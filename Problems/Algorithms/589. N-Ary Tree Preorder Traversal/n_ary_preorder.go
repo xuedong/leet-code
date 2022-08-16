7ffbe9f7398a43eeebbf71656a3b2b111592bc4d
xuedong <shang.xuedong@yahoo.fr>
2022-08-16 17:23:27 +0800

    Format golang codes

--- Problems/Algorithms/589. N-Ary Tree Preorder Traversal/n_ary_preorder.go
+++ Problems/Algorithms/589. N-Ary Tree Preorder Traversal/n_ary_preorder.go
@@ -1,23 +1,23 @@
-/**
- * Definition for a Node.
- * type Node struct {
- *     Val int
- *     Children []*Node
- * }
- */
+package main
+
+// Definition for a Node.
+type Node struct {
+	Val      int
+	Children []*Node
+}
 
 func preorder(root *Node) []int {
-    if root == nil {
-        return []int{}
-    }
+	if root == nil {
+		return []int{}
+	}
 
-    if root.Children == nil {
-        return []int{root.Val}
-    }
+	if root.Children == nil {
+		return []int{root.Val}
+	}
 
-    ans := []int{root.Val}
-    for _, child := range root.Children {
-        ans = append(ans, preorder(child)...)
-    }
-    return ans
+	ans := []int{root.Val}
+	for _, child := range root.Children {
+		ans = append(ans, preorder(child)...)
+	}
+	return ans
 }
