61cd0159bd7a71c36ff1fd9a52a8e72431c2c413
xuedong <shang.xuedong@yahoo.fr>
2022-09-08 12:31:45 +0800

    Add 396

---
+++ Problems/Algorithms/396. Rotate Function/rotate_function.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxRotateFunction(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        ans, total = 0, 0
+        for i in range(n):
+            ans += i * nums[i]
+            total += nums[i]
+
+        curr = ans
+        for i in range(n-1, 0, -1):
+            curr += total - nums[i]
+            curr -= (n-1) * nums[i]
+            if curr > ans:
+                ans = curr
+
+        return ans
