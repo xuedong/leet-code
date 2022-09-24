a8a9e197b84f57f352f211e2e44159e4e61069b9
xuedong <shang.xuedong@yahoo.fr>
2022-09-24 10:56:56 +0800

    Add 781

---
+++ Problems/Algorithms/781. Rabbits in Forest/rabbits_in_forest.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        counts = Counter(answers)
+        ans = 0
+        for key, value in counts.items():
+            num = value // (key + 1)
+            num = num + 1 if value % (key + 1) != 0 else num
+            ans += num * (key + 1)
+        return ans
