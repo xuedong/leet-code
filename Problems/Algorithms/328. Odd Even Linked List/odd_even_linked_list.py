15d3900ff14b2e04304aba4da2ecb03bed216c82
xuedong <shang.xuedong@yahoo.fr>
2022-12-08 22:19:36 +0800

    Add some python solutions

---
+++ Problems/Algorithms/328. Odd Even Linked List/odd_even_linked_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        odd, even = head, head.next
+        even_head = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = even_head
+        return head
---
+++ Problems/Algorithms/876. Middle of the Linked List/middle_linked_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
