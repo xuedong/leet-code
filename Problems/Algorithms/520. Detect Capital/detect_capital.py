5fd73a1408ecb5ef55b8512972205e418f652f2f
xuedong <shang.xuedong@yahoo.fr>
2023-01-02 12:09:24 +0800

    Add 520

---
+++ Problems/Algorithms/520. Detect Capital/detect_capital.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return word.islower() or word.isupper() or word[0].isupper() and word[1:].islower()
