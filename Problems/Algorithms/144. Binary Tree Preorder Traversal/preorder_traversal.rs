a1c8788a91d0f655f1b1c7d13f6d57586a31594c
xuedong <shang.xuedong@yahoo.fr>
2023-01-09 21:34:54 +0800

    Add some python and rust solutions

---
+++ Problems/Algorithms/144. Binary Tree Preorder Traversal/preorder_traversal.rs
@@ -0,0 +1,33 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut result: Vec<i32> = vec![];
+        if let Some(node) = root {
+            result.push(node.borrow().val);
+            result.append(&mut Self::preorder_traversal(node.borrow().left.clone()));
+            result.append(&mut Self::preorder_traversal(node.borrow().right.clone()));
+        }
+
+        result
+    }
+}
