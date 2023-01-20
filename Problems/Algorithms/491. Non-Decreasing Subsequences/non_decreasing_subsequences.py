e46903165ce6ed4c777c069e69ecd955f316f5e6
xuedong <shang.xuedong@yahoo.fr>
2023-01-20 23:05:18 +0800

    Add 491

---
+++ Problems/Algorithms/491. Non-Decreasing Subsequences/non_decreasing_subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+        current = []
+
+        def helper(index):
+            if index == len(nums):
+                if len(current) >= 2:
+                    result.add(tuple(current))
+                return
+
+            if not current or current[-1] <= nums[index]:
+                current.append(nums[index])
+                helper(index+1)
+                current.pop()
+            helper(index+1)
+
+        helper(0)
+        return result
