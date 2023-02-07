62d4712db0d0963c585a1d4e71f5031ce6b911d3
xuedong <shang.xuedong@yahoo.fr>
2023-02-07 22:43:56 +0800

    Add 904

---
+++ Problems/Algorithms/904. Fruit into Baskets/fruit_into_baskets.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        basket = {}
+        left = 0
+        
+        for right, fruit in enumerate(fruits):
+            basket[fruit] = basket.get(fruit, 0) + 1
+
+            if len(basket) > 2:
+                basket[fruits[left]] -= 1
+
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+        
+        return right - left + 1
