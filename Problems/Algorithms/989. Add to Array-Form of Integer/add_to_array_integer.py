bf4440c891b3537ec39759ced3a165a787ae5237
xuedong <shang.xuedong@yahoo.fr>
2023-02-15 22:17:05 +0800

    Add 989

---
+++ Problems/Algorithms/989. Add to Array-Form of Integer/add_to_array_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        for i in range(len(num) - 1, -1, -1):
+            k, num[i] = divmod(num[i] + k, 10)
+        return [int(i) for i in str(k)] + num if k else num
