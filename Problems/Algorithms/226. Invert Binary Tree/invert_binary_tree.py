88ea37b54921a88dbb3d23c0af9e27b51d2fa223
xuedong <shang.xuedong@yahoo.fr>
2023-02-18 22:15:00 +0800

    Add some python solutions

---
+++ Problems/Algorithms/226. Invert Binary Tree/invert_binary_tree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        left, right = self.invertTree(root.right), self.invertTree(root.left)
+        root.left, root.right = left, right
+        return root
