bbd71521406afa87467a3b26495f8af004ba60ee
xuedong <shang.xuedong@yahoo.fr>
2023-03-16 22:40:52 +0800

    Add some python solutions

---
+++ Problems/Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/inorder_postorder_construction.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
+        
+        val = postorder[-1]
+        root = TreeNode(val)
+
+        index = inorder.index(val)
+        root.left = self.buildTree(inorder[:index], postorder[:index])
+        root.right = self.buildTree(inorder[index+1:], postorder[index:-1])
+
+        return root
