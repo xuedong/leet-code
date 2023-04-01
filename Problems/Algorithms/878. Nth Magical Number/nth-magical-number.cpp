e3ed30e38e944b57c7f2b55b85e90bef50cfac71
xuedong <shang.xuedong@yahoo.fr>
2023-04-01 13:22:28 +0800

    Add 878

---
+++ Problems/Algorithms/878. Nth Magical Number/nth-magical-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        int MOD = 1e9 + 7;
+        int lcm = a * b / gcd(a, b);
+
+        long left = -1;
+        long right = (long) n * min(a, b) + 1;
+        while (right - left > 1) {
+            long mid = left + (right - left) / 2;
+
+            if (mid / a + mid / b - mid / lcm < n) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return (int) (right % MOD);
+    }
+
+    int gcd(int x, int y) {
+        if (x == 0) {
+            return y;
+        }
+        return gcd(y % x, x);
+    }
+};
