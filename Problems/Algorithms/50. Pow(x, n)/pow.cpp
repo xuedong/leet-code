daff385eaa97977adaec4a096dbe48785b1de1cc
xuedong <shang.xuedong@yahoo.fr>
2023-07-24 23:14:22 +0800

    Add some c++ solutions

---
+++ Problems/Algorithms/50. Pow(x, n)/pow.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+class Solution {
+public:
+    double binaryExp(double x, long long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -1 * n);
+        }
+
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        return binaryExp(x, (long long) n);
+    }
+};
