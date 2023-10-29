dec2e5191a042a3b564f7dc481bf54a668cb5cbc
xuedong <shang.xuedong@yahoo.fr>
2023-10-29 13:10:43 +0800

    Add some cpp solutions

---
+++ Problems/Algorithms/458. Poor Pigs/poor-pigs.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log2(buckets) / log2(int(minutesToTest/minutesToDie) + 1));
+    }
+};
