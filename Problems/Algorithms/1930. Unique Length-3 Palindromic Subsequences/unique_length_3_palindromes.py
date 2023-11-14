56d34a49f79851ba43aaf85821f316c685c6372a
xuedong <shang.xuedong@yahoo.fr>
2023-11-15 00:15:34 +0900

    Add 1930

---
+++ Problems/Algorithms/1930. Unique Length-3 Palindromic Subsequences/unique_length_3_palindromes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        letters = set(s)
+
+        ans = 0
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+
+            for k in range(i+1, j):
+                between.add(s[k])
+
+            ans += len(between)
+
+        return ans
