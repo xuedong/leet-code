cc70ba698d8b7acdff66634a5a26073549b9146b
xuedong <shang.xuedong@yahoo.fr>
2023-12-09 12:31:19 +0800

    Add some cpp solutions

---
+++ Problems/Algorithms/94. Binary Tree Inorder Traversal/inorder-traversal.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int>& result) {
+        if (root != nullptr) {
+            helper(root->left, result);
+            result.push_back(root->val);
+            helper(root->right, result);
+        }
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helper(root, result);
+        return result;
+    }
+};
