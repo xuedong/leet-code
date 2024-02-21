8d97b7c57c4aeb56babc4f3a3057752354caa62d
xuedong <shang.xuedong@yahoo.fr>
2024-02-21 13:39:43 +0800

    Add some cpp solutions

---
+++ Problems/Algorithms/201. Bitwise AND of Numbers Range/bitwise-range.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            count += 1;
+        }
+
+        return left << count;
+    }
+};
---
+++ Problems/Algorithms/268. Missing Number/missing-number.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            ans = ans ^ i;
+        }
+        for(int i = 0; i < n; i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
