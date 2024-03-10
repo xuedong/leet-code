8eea57559047dc9a1f469859acb622a2552aaf67
xuedong <shang.xuedong@yahoo.fr>
2024-03-10 10:48:40 +0800

    Add 349

---
+++ Problems/Algorithms/349. Intersection of Two Arrays/intersection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1).intersection(set(nums2))
