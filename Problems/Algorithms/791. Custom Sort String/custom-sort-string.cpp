7354f43d91bb111666c460ce7b558ed40ae28e53
xuedong <shang.xuedong@yahoo.fr>
2024-03-11 23:00:54 +0800

    Add 791

---
+++ Problems/Algorithms/791. Custom Sort String/custom-sort-string.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        sort(s.begin(), s.end(), [order](char c1, char c2) {
+            return order.find(c1) < order.find(c2);
+        });
+
+        return s;
+    }
+};
