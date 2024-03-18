435c7c240bc94b4e80af6d9a1aab6b8ee681ff35
xuedong <shang.xuedong@yahoo.fr>
2024-03-18 23:04:44 +0800

    Add some python solutions

---
+++ Problems/Algorithms/452. Minimum Number of Arrows to Burst Balloons/min_number_arrows.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[0])
+
+        arrows = 1
+        end = points[0][1]
+        for balloon in points[1:]:
+            if balloon[0] > end:
+                arrows += 1
+                end = balloon[1]
+            else:
+                end = min(end, balloon[1])
+
+        return arrows
