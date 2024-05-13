65869933b7d0be2d1f4d35c530b4d369b739935c
xuedong <shang.xuedong@yahoo.fr>
2024-05-13 21:53:11 +0800

    Add 861

---
+++ Problems/Algorithms/861. Score after Flipping Matrix/score_after_flipping.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from typing import list
+
+
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            if grid[i][0] == 0:
+                for j in range(n):
+                    grid[i][j] ^= 1
+
+        for j in range(1, n):
+            count = 0
+            for i in range(m):
+                if grid[i][j] == 0:
+                    count += 1
+            if count > m - count:
+                for i in range(m):
+                    grid[i][j] ^= 1
+
+        score = 0
+        for i in range(m):
+            for j in range(n):
+                column = grid[i][j] << (n-j-1)
+                score += column
+
+        return score
