12d5b7d87f3dee7a8eb71a8d48e173b2f8052d73
xuedong <shang.xuedong@yahoo.fr>
2024-06-10 19:28:08 +0800

    Add 1051

---
+++ Problems/Algorithms/1051. Height Checker/height_checker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        
+        ans = 0
+        for i in range(len(expected)):
+            if heights[i] != expected[i]:
+                ans += 1
+        return ans
