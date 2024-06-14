b7cdfb0d7d297725dcd4a700fd07a861573c3e64
xuedong <shang.xuedong@yahoo.fr>
2024-06-15 00:52:46 +0800

    Add 945

---
+++ Problems/Algorithms/945. Minimum Increment to Make Array Unique/min_increment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+
+        ans = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                incr = nums[i-1] + 1 - nums[i]
+                ans += incr
+
+                nums[i] = nums[i-1] + 1
+
+        return ans
