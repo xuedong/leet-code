3c6e41588a0c309747dd6d357a393c9be32e413b
xuedong <shang.xuedong@yahoo.fr>
2024-06-22 10:23:50 +0800

    Add 1248

---
+++ Problems/Algorithms/1248. Count Number of Nice Subarrays/nice_subarrays.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        curr_sum = 0
+        subarrays = 0
+        prefix_sum = {curr_sum: 1}
+
+        for i in range(len(nums)):
+            curr_sum += nums[i] % 2
+            if curr_sum - k in prefix_sum:
+                subarrays += prefix_sum[curr_sum - k]
+            prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
+
+        return subarrays
