8bb8043c32a6b9c02d2773752d00614de001db42
xuedong <shang.xuedong@yahoo.fr>
2024-09-17 22:43:25 +0800

    Add 884

---
+++ Problems/Algorithms/884. Uncommon Words from Two Sentences/uncommon_words.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        count = {}
+
+        for word in s1.split():
+            count[word] = count.get(word, 0) + 1
+        for word in s2.split():
+            count[word] = count.get(word, 0) + 1
+
+        return [word for word in count if count[word] == 1]
