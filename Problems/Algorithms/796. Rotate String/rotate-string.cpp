f9b749d52a771f4dccf81b73a3e6b345fe21f044
xuedong <shang.xuedong@yahoo.fr>
2024-11-03 12:17:40 +0800

    Add 796

---
+++ Problems/Algorithms/796. Rotate String/rotate-string.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        string doubledString = s + s;
+        return doubledString.find(goal) < doubledString.length();
+    }
+};
