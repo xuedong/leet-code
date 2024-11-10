17e0d6b5b25181248de155376af53891bd941cc7
xuedong <shang.xuedong@yahoo.fr>
2024-11-10 19:48:01 +0800

    add 3133

---
+++ Problems/Algorithms/3133. Minimum Array End/min-array-end.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long ans = x;
+
+        while (--n) {
+            ans = (ans + 1) | x;
+        }
+
+        return ans;
+    }
+};
