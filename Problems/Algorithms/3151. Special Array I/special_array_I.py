31feba7a192692171e79edcc9f2de9b61a415096
xuedong <shang.xuedong@yahoo.fr>
2025-02-01 12:32:37 +0800

    Add 3151

---
+++ Problems/Algorithms/3151. Special Array I/special_array_I.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        for i in range(len(nums)-1):
+            if nums[i] % 2 == nums[i+1] % 2:
+                return False
+        return True
