7880bbceaabeccf12a9a35fbe5748cfea4b65263
xuedong <shang.xuedong@yahoo.fr>
2025-03-30 14:42:10 +0800

    Add some python solutions

---
+++ Problems/Algorithms/763. Partition Labels/partition_labels.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from typing immport List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_occurrence = [0] * 26
+        for i, ch in enumerate(s):
+            last_occurrence[ord(ch) - ord("a")] = i
+
+        start, end = 0, 0
+        sizes = []
+
+        for i, ch in enumerate(s):
+            end = max(end, last_occurrence[ord(ch) - ord("a")])
+
+            if i == end:
+                sizes.append(i - start + 1)
+                start = i + 1
+
+        return sizes
