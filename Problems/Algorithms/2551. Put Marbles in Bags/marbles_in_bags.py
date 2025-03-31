bb1fba616e36af3e0ef98d2e28a9fe73ceb16b16
xuedong <shang.xuedong@yahoo.fr>
2025-03-31 22:46:38 +0800

    Add some python solutions

---
+++ Problems/Algorithms/2551. Put Marbles in Bags/marbles_in_bags.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        n = len(weights)
+        pairs = [weights[i] + weights[i+1] for i in range(n-1)]
+
+        pairs.sort()
+
+        ans = 0
+        for i in range(k-1):
+            ans += pairs[n-2-i] - pairs[i]
+
+        return ans
