c55e3c7707c045f1ae4ffc371d7d0979b35102d0
xuedong <shang.xuedong@yahoo.fr>
2025-06-20 23:53:43 +0800

    Add 3443

---
+++ Problems/Algorithms/3443. Maximum Manhattan Distance after K Changes/max_manhattan_distance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        latitude, longitude = 0, 0
+        
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            if s[i] == "N":
+                latitude += 1
+            elif s[i] == "S":
+                latitude -= 1
+            elif s[i] == "E":
+                longitude += 1
+            elif s[i] == "W":
+                longitude -= 1
+            ans = max(ans, min(abs(latitude) + abs(longitude) + k * 2, i + 1))
+        return ans
