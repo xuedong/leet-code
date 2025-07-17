c9094c5486011ebd0a8aa182370e19119c9673c7
xuedong <shang.xuedong@yahoo.fr>
2025-07-17 22:24:11 +0800

    Add 3202

---
+++ Problems/Algorithms/3202. Find the Maximum Length of Valid Subsequence II/find_max_length_valid_sequences_II.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+        dp = [[0] * k for _ in range(k)]
+        res = 0
+        for num in nums:
+            num %= k
+            for prev in range(k):
+                dp[prev][num] = dp[num][prev] + 1
+                res = max(res, dp[prev][num])
+        return res
