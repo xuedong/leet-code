fb3c38c87e21b9e53f5345cbc2290f2fdd6c6a04
xuedong <shang.xuedong@yahoo.fr>
2025-09-21 13:31:11 +0800

    Add 1912

---
+++ Problems/Algorithms/1912. Design Movie Rental System/design_movie_rental_system.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from sortedcontainers import SortedList
+
+
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        self.available = {}
+        self.rented = SortedList()
+        self.price_mapping = {(shop, movie): price for shop, movie, price in entries}
+
+        for shop, movie, price in entries:
+            if movie not in self.available:
+                self.available[movie] = SortedList()
+            self.available[movie].add((price, shop))
+
+    def search(self, movie: int) -> List[int]:
+        return [shop for _, shop in self.available.get(movie, [])[:5]]
+
+    def rent(self, shop: int, movie: int) -> None:
+        price = self.price_mapping[(shop, movie)]
+        self.available[movie].remove((price, shop))
+        self.rented.add((price, shop, movie))
+
+    def drop(self, shop: int, movie: int) -> None:
+        price = self.price_mapping[(shop, movie)]
+        self.rented.remove((price, shop, movie))
+        self.available[movie].add((price, shop))
+
+    def report(self) -> List[List[int]]:
+        return [[shop, movie] for _, shop, movie in self.rented[:5]]
+
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
